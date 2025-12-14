b69abce7d059ebfaede3e461a2d1971c0c7b3d51
Lucas Arantes <108706950+Arantees@users.noreply.github.com>
2025-12-14 19:05:27 -0300

    Add authenticated HTML frontend for login and loans

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get("/", (req, res) => {
   res.json({ message: "API funcionando " });
