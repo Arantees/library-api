b3ae7700fa99fd1ca071cdf32df7b4deaee89964
Lucas Arantes <108706950+Arantees@users.noreply.github.com>
2025-12-14 17:08:55 -0300

    Implement rental value calculation for loans

--- src/controllers/loan.controller.js
+++ src/controllers/loan.controller.js
@@ -1,4 +1,5 @@
 const loanService = require("../services/loan.service");
+const calculationService = require("../services/calculation.service");
 
 function createLoan(req, res) {
   const { userName, bookTitle, days } = req.body;
@@ -26,7 +27,46 @@ function getLoan(req, res) {
   return res.json(loan);
 }
 
+function getRentalValue(req, res) {
+  const loan = loanService.findLoanById(req.params.id);
+
+  if (!loan) {
+    return res.status(404).json({ message: "Loan not found" });
+  }
+
+  const value = calculationService.calculateRental(loan.days);
+
+  return res.json({ rentalValue: value });
+}
+
+function getFineValue(req, res) {
+  const loan = loanService.findLoanById(req.params.id);
+
+  if (!loan) {
+    return res.status(404).json({ message: "Loan not found" });
+  }
+
+  const today = new Date();
+
+  if (today <= loan.dueDate) {
+    return res.json({ fine: 0 });
+  }
+
+  const daysLate = Math.ceil((today - loan.dueDate) / (1000 * 60 * 60 * 24));
+
+  let fine = calculationService.calculateFine(daysLate);
+
+  if (!loan.hasLateBefore) {
+    fine = calculationService.applyFirstDelayDiscount(fine);
+    loan.hasLateBefore = true;
+  }
+
+  return res.json({ fine });
+}
+
 module.exports = {
   createLoan,
   getLoan,
+  getRentalValue,
+  getFineValue,
 };
